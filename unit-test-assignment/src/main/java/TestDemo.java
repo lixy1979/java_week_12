28048867bdca7818413adb68e9de27c7652cd1ce
lixy1979 <120543146+lixy1979@users.noreply.github.com>
2023-03-25 10:57:06 -0400

    first commit

---
+++ unit-test-assignment/src/main/java/TestDemo.java
@@ -0,0 +1,25 @@
+import java.util.Random;
+
+public class TestDemo {
+
+	public int addPositive(int a, int b) {
+		
+		if (a <= 0 || b <= 0) {
+			throw new IllegalArgumentException("Both parameters must be positive!");
+		}
+		
+		return a + b;
+	}
+	
+	public int randomNumberSquared() {
+		int a = getRandomInt();
+		int squA = a*a;
+		System.out.printf("randomNumberSquared %d", squA);
+		return squA;	
+	}
+	
+	int getRandomInt() {
+		  Random random = new Random();
+		  return random.nextInt(10) + 1;
+		}
+}
